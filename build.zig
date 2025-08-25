48a7e1e1844a9be227073bba9ec7a3ed2a0ce360
Meghan Denny <hello@nektro.net>
2025-08-24 21:13:08 -0700

    bring back fuzzing

--- build.zig
+++ build.zig
@@ -44,33 +44,28 @@ pub fn build(b: *std.Build) void {
 }
 
 fn addFuzzer(b: *std.Build, target: std.Build.ResolvedTarget, comptime name: []const u8, afl_clang_args: []const []const u8) *std.Build.Step.InstallFile {
-    // The library
     const fuzz_lib = b.addStaticLibrary(.{
-        .name = "fuzz-" ++ name,
+        .name = "fuzz-" ++ name ++ "-lib",
         .root_source_file = b.path("fuzz/main.zig"),
         .target = target,
         .optimize = .Debug,
     });
     fuzz_lib.want_lto = true;
     fuzz_lib.bundle_compiler_rt = true;
-    // Seems to be necessary for LLVM >= 15
+    fuzz_lib.use_llvm = true;
+    fuzz_lib.use_lld = true;
     fuzz_lib.root_module.pic = true;
 
     deps.addAllTo(fuzz_lib);
 
-    // Setup the output name
     const fuzz_executable_name = "fuzz-" ++ name;
-    const fuzz_exe_path = std.fs.path.join(b.allocator, &.{ b.cache_root.path.?, fuzz_executable_name }) catch @panic("OOM");
 
-    // We want `afl-clang-lto -o path/to/output path/to/library`
-    const fuzz_compile = b.addSystemCommand(&.{ "afl-clang-lto", "-v", "-o", fuzz_exe_path });
-    // Add the path to the library file to afl-clang-lto's args
+    const fuzz_compile = b.addSystemCommand(&.{ "afl-clang-lto", "-v", "-o" });
+    const output_path = fuzz_compile.addOutputFileArg(fuzz_executable_name);
     fuzz_compile.addArtifactArg(fuzz_lib);
-    // Custom args
     fuzz_compile.addArgs(afl_clang_args);
 
-    // Install the cached output to the install 'bin' path
-    const fuzz_install = b.addInstallBinFile(.{ .cwd_relative = fuzz_exe_path }, fuzz_executable_name);
+    const fuzz_install = b.addInstallBinFile(output_path, fuzz_executable_name);
     fuzz_install.step.dependOn(&fuzz_compile.step);
 
     return fuzz_install;
--- fuzz/main.zig
+++ fuzz/main.zig
@@ -1,17 +1,14 @@
 const std = @import("std");
 const json = @import("json");
+const nfs = @import("nfs");
 
 pub export fn main() void {
-    // Setup an allocator that will detect leaks/use-after-free/etc
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // this will check for leaks and crash the program if it finds any
     defer std.debug.assert(gpa.deinit() == .ok);
     const allocator = gpa.allocator();
 
-    // Read the data from stdin
-    const stdin = std.io.getStdIn();
+    const stdin = nfs.stdin();
 
-    // Try to parse the data
-    var parsed = json.parse(allocator, "[stdin]", stdin.reader(), .{ .support_trailing_commas = true, .maximum_depth = 100 }) catch return;
+    var parsed = json.parse(allocator, "[stdin]", stdin, .{ .support_trailing_commas = true, .maximum_depth = 100 }) catch return;
     defer parsed.deinit(allocator);
 }
